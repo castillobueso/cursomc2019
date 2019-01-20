84ddbc1acaf2b1ff93eb1c6452dd36337549d2b5
castillobueso <castillobueso@yahoo.com>
2019-01-20 19:30:47 -0200

    Ajustes no endpoint /Categorias/{id}

--- src/main/java/com/hectorcastillo/cursomc/services/CategoriaService.java
+++ src/main/java/com/hectorcastillo/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.hectorcastillo.cursomc.domain.Categoria;
 import com.hectorcastillo.cursomc.repositories.CategoriaRepository;
+import com.hectorcastillo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto nao encontrado Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;		
 	}
 }
